369860dc09f33a9220dcdf25312317e5ad7d131d
Chris <ChrisFlynn1990@aol.com>
2011-06-17 08:56:05 -0700

    Main Class: First attempt

--- src/dod/Main.java
+++ src/dod/Main.java
@@ -3,19 +3,66 @@
  * and open the template in the editor.
  */
 
-package dod;
+package assignment2dod;
 
 /**
  *
  * @author 20312792
  */
+
+import java.io.*;
+
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
+
+
+
+
     public static void main(String[] args) {
-        // TODO code application logic here
+   // TODO code application logic here
+
+       BufferedReader r =  new BufferedReader( new InputStreamReader(System.in));
+       Player p = new Player();
+       World w = new World();
+
+        p.l = w.BabblingBrook;
+
+
+     String cmd="";
+     while(true){
+         try {
+         cmd = r.readLine();
+         
+         
+          } catch (Exception e){};
+
+  System.out.println(cmd);
+     
+     
+  if(cmd.equals("North"))   
+
+  p.goNorth();
+           
+  if(cmd.equals("South"))   
+     
+   p.goSouth();
+
+   if(cmd.equals("East"))   
+     
+    p.goEast();
+   
+  if(cmd.equals("West"))   
+     
+    p.goWest();
+  
+  
+     }
+
+     
     }
 
 }
